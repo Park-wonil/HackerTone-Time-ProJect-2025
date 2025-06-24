a34ad76ec4df648d2ccde3d4beb93cdfef292b45
Wonil Park <parkwonil0522@gmail.com>
2025-06-24 18:35:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,18 +10,25 @@ function showLogin() {
 function register() {
   const id = document.getElementById('signup-id').value;
   const pw = document.getElementById('signup-pw').value;
-  if (!id || !pw) {
-    alert('아이디와 비밀번호를 입력하세요!');
+  const nickname = document.getElementById('signup-nickname').value;
+  const birth = document.getElementById('signup-birth').value;
+
+  if (!id || !pw || !nickname || !birth) {
+    alert('모든 정보를 입력하세요!');
     return;
   }
   if (localStorage.getItem('user_' + id)) {
     alert('이미 존재하는 아이디입니다!');
     return;
   }
-  localStorage.setItem('user_' + id, pw);
+
+  const userData = JSON.stringify({ pw, nickname, birth });
+  localStorage.setItem('user_' + id, userData);
+
   alert('회원가입이 완료되었습니다!');
   showLogin();
 }
+
 
 function login() {
   const id = document.getElementById('login-id').value;
